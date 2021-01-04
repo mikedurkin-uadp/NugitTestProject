9bd8486fe7800effedc80b363abad6b9895dae58
durkinm <mike.durkin@uafoundation.org>
2021-01-04 10:59:00 -0700

    initial push

--- NugitTestProject/Program.cs
+++ NugitTestProject/Program.cs
@@ -6,7 +6,7 @@ namespace NugitTestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Github!");
         }
     }
 }
