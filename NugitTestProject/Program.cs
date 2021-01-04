59df26d70cd294bc5fc5342bd61307edb31f291d
durkinm <mike.durkin@uafoundation.org>
2021-01-04 11:01:20 -0700

    edit

--- NugitTestProject/Program.cs
+++ NugitTestProject/Program.cs
@@ -6,7 +6,7 @@ namespace NugitTestProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Github!");
+            Console.WriteLine("Hello again Github!");
         }
     }
 }
